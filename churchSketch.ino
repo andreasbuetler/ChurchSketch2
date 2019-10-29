4789f78326898ba8f5f18af186b21c675ae3a5a4
Anderas Bütler <andreas.buetler@zhdk.ch>
2019-10-29 23:26:31 +0100

    Update churchSketch.ino
    added PIR Read

--- churchSketch.ino
+++ churchSketch.ino
@@ -3,12 +3,12 @@
 #include <Arduino.h>
 #include <analogWrite.h>
 
-#define pirInputPin 25
-#define motorPinL 32
-#define motorPinR 27
-#define poti1InputPin 34
-#define poti2InputPin 35
-#define endStopPin 19
+#define pirInputPin     25
+#define motorPinL       32
+#define motorPinR       27
+#define poti1InputPin   34
+#define poti2InputPin   35
+#define endStopPin      19
 
 const int maxMotorTime = 5;
 int motorSpeed = 200;
@@ -17,40 +17,33 @@ boolean motorDirection;
 boolean movement = false;
 boolean endStop = false;
 
-//test
-
-
 
 
 void setup() {
   pinMode(endStopPin, INPUT);
-  //pinMode(motorPin, OUTPUT);
+  pinMode(pirInputPin, INPUT);
   initializing();
   Serial.begin(9600);
-
 }
 
 void loop() {
-  //setMotorTime();
-  //setMotorSpeed();
-  //endStopper();
-  //if(endStop == false){
-  driveMotor(motorDirection, motorTime, motorSpeed);
-  //Serial.println("driven");
-  //driveMotor(motorDirection,0,0);
-  //delay(3000);
-
-  motorDirection = !motorDirection;
-  Serial.println(motorDirection);
-  driveMotor(motorDirection, motorTime, motorSpeed);
-
- //driveMotor(1,3,100);
- //driveMotor(0,3,100);
-
-  //}
-
-  //Serial.println(endStop);
-
+  movement = pirRead(pirInputPin);
+  if (movement){
+    //Serial.println("MOVEMENT!");
+    }
+  setMotorTime();
+  setMotorSpeed();
+  endStopper();
+  if (endStop == false) {
+    driveMotor(motorDirection, motorTime, motorSpeed);
+    delay(3000);
+    motorDirection = !motorDirection;
+    driveMotor(motorDirection, motorTime, motorSpeed);
+  } else {
+    motorDirection = !motorDirection;
+    driveMotor(motorDirection, 0, 0);
+  }
+  Serial.println(digitalRead(endStopPin));
 }
 
 
@@ -63,38 +56,36 @@ void endStopper() {
 }
 
 void setMotorTime() {
-  motorTime = analogRead(poti1InputPin);//map(analogRead(poti1InputPin),0,4095,0,maxMotorTime);
+  motorTime = map(analogRead(poti1InputPin), 0, 4095, 0, maxMotorTime);
 }
 void setMotorSpeed() {
   motorSpeed = map(analogRead(poti2InputPin), 0, 4095, 0, 255);
 }
 
 void initializing() {
-
 }
 
 void driveMotor(boolean dir, int duration, int tempo) {
-  //Serial.println(dir);
-
   if (dir == true) {
-    Serial.println("LINKS");
-    for (int i = 0; i <= duration*1000; i++) {
-      analogWrite(motorPinL,tempo);
+    for (int i = 0; i <= duration * 1000; i++) {
+      analogWrite(motorPinL, tempo);
       analogWrite(motorPinR, 0);
       delay(1);
     }
-  }else if (dir == false){
-    Serial.println("RECHTS");
-    for (int k = 0; k <= duration*1000; k++) {
+  } else if (dir == false) {
+    for (int k = 0; k <= duration * 1000; k++) {
       analogWrite(motorPinR, tempo);
       analogWrite(motorPinL, 0);
       delay(1);
-      
     }
   }
-  //tempo = map(tempo,0,4092,0,255);
-
-
 }
 
+boolean pirRead(int PinToRead) {
+  if (digitalRead(PinToRead) == LOW) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
