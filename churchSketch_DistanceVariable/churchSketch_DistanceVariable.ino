e146749467031cc0ee1de85bf77e9e1323754db5
Anderas Bütler <andreas.buetler@zhdk.ch>
2019-11-11 13:27:52 +0100

    added MotorStop, added EndStop & setStartPosition

--- churchSketch_DistanceVariable/churchSketch_DistanceVariable.ino
+++ churchSketch_DistanceVariable/churchSketch_DistanceVariable.ino
@@ -1,3 +1,14 @@
+/* Things to implement/problems:
+    - endStopper: Tempo, Stops when Endstopper == true
+    - reading pir when on the Top >> if movmement add delay
+    - set position after endStop() == true
+    - set final variables
+*/
+
+
+
+
+
 #include <dummy.h>
 
 #include <Arduino.h>
@@ -11,12 +22,12 @@
 #define endStopPin      25
 
 const int maxMotorDistance = 5000;
-const int motorTime = 5;
+const int motorTime = 10;
 const int waitingTime = 3;
 
 int motorSpeed = 2000;
 int motorDistance  = 1000;
-boolean motorDirection = true; 
+boolean motorDirection = true;
 boolean movement = false;
 boolean endStop = false;
 
@@ -33,14 +44,14 @@ void setup() {
 void loop() {
   endStopper();
   //Serial.println(endStop);
-  
+
   movement = pirRead(pirInputPin);
   if (movement) {
     Serial.println("MOVEMENT!");
     motorDirection = true;
     BalloonRiseAndFall();
-    delay(waitingTime*1000);
-    
+    delay(waitingTime * 1000);
+
   }
 }
 
@@ -59,11 +70,18 @@ void endStopper() {
 void setDistance() {
   motorDistance = map(analogRead(poti1InputPin), 0, 4095, 0, maxMotorDistance);
 }
-void setMotorSpeed() {
-  motorSpeed = map(analogRead(poti1InputPin), 0, 4095, 0, 255);
-}
+//void setMotorSpeed() {
+//  motorSpeed = map(analogRead(poti1InputPin), 0, 4095, 0, 255);
+//}
 
 void initializing() {
+  Serial.println("initializing....");
+  driveMotor(1, 3, 200);
+  //driveMotor(0,3,200);
+  stopMotor();
+  while (endStop == false) {
+    driveMotor(0, 1, 200);
+  }
 }
 
 void driveMotor(boolean dir, int duration, int tempo) {
@@ -75,7 +93,7 @@ void driveMotor(boolean dir, int duration, int tempo) {
       if (endStop == true) {
         i = duration;
       }
-      
+
       analogWrite(motorPinR, 0);
       analogWrite(motorPinL, tempo);
       delay(1);
@@ -91,34 +109,22 @@ void driveMotor(boolean dir, int duration, int tempo) {
       delay(1);
     }
   }
-  if(endStop == true){
-    for (int r = 0; r <= 200; r++) {
-      if (endStop == true) {
-        r = 200;
-      }
-      analogWrite(motorPinL, 0);
-      analogWrite(motorPinR, 20);
-      //delay(1);
-    }   
-  }
 }
 
 void BalloonRiseAndFall() {
   Serial.println("START TO RISE");
   setDistance();
-  
   motorSpeed = calculateSpeed(motorTime, motorDistance);
   Serial.println(motorSpeed);
   driveMotor(motorDirection, motorTime, motorSpeed);
+  stopMotor();
   delay(waitingTime * 1000);
   Serial.println("START TO FALL");
   motorDirection = !motorDirection;
   driveMotor(motorDirection, motorTime, motorSpeed);
-  driveMotor(motorDirection, 0, 0);
+  stopMotor();
   Serial.println("BACK TO BOTTOM");
   motorDirection = !motorDirection;
-  
-
 }
 
 boolean pirRead(int PinToRead) {
@@ -132,5 +138,17 @@ boolean pirRead(int PinToRead) {
 int calculateSpeed(int duration, int distance) {
   int calculatedSpeed = distance / duration;
   Serial.println(calculatedSpeed);
-  return map(calculatedSpeed,0,maxMotorDistance/duration,0,255);
+  return map(calculatedSpeed, 0, maxMotorDistance / duration, 0, 255);
 }
+
+void stopMotor() {
+  driveMotor(motorDirection, 0, 0);
+}
+
+void setStartPosition() {
+  if (endStop) {
+    driveMotor(1, 1, 200);
+  }
+}
+
+
